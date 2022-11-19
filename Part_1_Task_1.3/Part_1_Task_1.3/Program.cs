1ee9aeb07d12b7b8f14552f85e317d483ad7ab0b
BshrvEgr <118613546+BshrvEgr@users.noreply.github.com>
2022-11-19 16:40:46 +0300

    Add files via upload

---
+++ Part_1_Task_1.3/Part_1_Task_1.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Part_1_Task_1._3
+{
+    internal class Program
+    {
+        static void PrintNumber()
+        {
+            try
+            {
+                Console.Write("Введите число: ");
+                double number = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Вы ввели число: {0}", number);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Thread.Sleep(1000);
+                Console.Clear();
+                PrintNumber();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrintNumber();
+            Console.ReadKey();
+        }
+
+    }
+}
