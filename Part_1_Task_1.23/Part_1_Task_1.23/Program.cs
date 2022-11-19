91dbdb70cf95f9a8357b6b6dac1a3560cfd35efa
BshrvEgr <118613546+BshrvEgr@users.noreply.github.com>
2022-11-19 17:31:50 +0300

    Add files via upload

---
+++ Part_1_Task_1.23/Part_1_Task_1.23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Part_1_Task_1._23
+{
+    internal class Program
+    {
+        static double GetAnswer(double number) => (Math.Pow(number, 2) + 10) /
+            Math.Sqrt(Math.Pow(number, 2) + 1);
+
+        static void Main(string[] args)
+        {
+            double a;
+            Console.Write("Введите число(a): ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ответ: {0}", GetAnswer(a));
+            Console.ReadKey();
+        }
+
+    }
+}
